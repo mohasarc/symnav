04816db777797bade954d814946804f4b8d091b7
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2026-05-08 01:08:55 +0300

    Add BackendError hierarchy in @symnav/core

--- packages/core/src/errors.ts
+++ packages/core/src/errors.ts
@@ -7,3 +7,47 @@ export class NotInWorkspaceError extends Error {
     this.startDir = startDir;
   }
 }
+
+export class BackendError extends Error {
+  readonly displayedPath: string;
+
+  constructor(message: string, displayedPath: string) {
+    super(message);
+    this.name = "BackendError";
+    this.displayedPath = displayedPath;
+  }
+}
+
+export class FileNotFoundError extends BackendError {
+  constructor(displayedPath: string) {
+    super(`File not found: ${displayedPath}`, displayedPath);
+    this.name = "FileNotFoundError";
+  }
+}
+
+export class OutsideWorkspaceError extends BackendError {
+  readonly workspaceRoot: string;
+
+  constructor(displayedPath: string, workspaceRoot: string) {
+    super(`Path is outside the workspace (${workspaceRoot}): ${displayedPath}`, displayedPath);
+    this.name = "OutsideWorkspaceError";
+    this.workspaceRoot = workspaceRoot;
+  }
+}
+
+export class IgnoredFileError extends BackendError {
+  constructor(displayedPath: string) {
+    super(`${displayedPath} is ignored by .gitignore`, displayedPath);
+    this.name = "IgnoredFileError";
+  }
+}
+
+export class UnsupportedFileError extends BackendError {
+  readonly extension: string;
+
+  constructor(displayedPath: string, extension: string) {
+    super(`Unsupported file extension: ${extension}`, displayedPath);
+    this.name = "UnsupportedFileError";
+    this.extension = extension;
+  }
+}

8e2bded8d2e15cd957eb4e10b170bef1fa73b87f
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2026-05-08 01:09:43 +0300

    Re-export backend surface from @symnav/core entry

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,4 +4,13 @@ export type { WorkspaceFileSystem } from "./file-system.js";
 export { nodeFileSystem } from "./file-system.js";
 export type { Workspace, CreateWorkspaceOptions } from "./workspace.js";
 export { createWorkspace } from "./workspace.js";
-export { NotInWorkspaceError } from "./errors.js";
+export type { LanguageBackend } from "./backend.js";
+export { BackendRouter } from "./backend.js";
+export {
+  NotInWorkspaceError,
+  BackendError,
+  FileNotFoundError,
+  OutsideWorkspaceError,
+  IgnoredFileError,
+  UnsupportedFileError,
+} from "./errors.js";

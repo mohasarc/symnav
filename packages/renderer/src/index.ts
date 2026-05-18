160bba6b3ef84d1a0f19fcd68af3ebb45db1b7b1
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2026-05-19 01:58:36 +0300

    export resolve renderer from @symnav/renderer

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -1,2 +1,4 @@
 export { renderOverviewText } from "./overview/render-overview-text.js";
 export { renderOverviewJson } from "./overview/render-overview-json.js";
+export { renderResolveText } from "./resolve/render-resolve-text.js";
+export { renderResolveJson } from "./resolve/render-resolve-json.js";
